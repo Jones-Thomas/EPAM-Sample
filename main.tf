2c3a824e1ee74d31b2493e81d269594a8e48fc4c
Jones-Thomas <jonesthomas.work@gmail.com>
2022-09-27 15:16:51 +0530

    Added terraform file

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 /*
  In order to connect to AWS. Terraform has to successfully authenticate. 
- It is done with the help of Programmatic API Keys (Access Key and Secret.)
+ It is done with the help of Programmatic API Keys (i.e) Access Key and Secret.
  Either you should save these Keys as Environment variables (or) save it as a AWS Config profile (AWS CLI).
  $ export AWS_ACCESS_KEY_ID=AK************IEVXQ
  $ export AWS_SECRET_ACCESS_KEY=gbaIbK*********************iwN0dGfS
@@ -62,3 +62,53 @@ Execute the command terraform plan to check what change would be made( Should al
 If you are happy with the changes it is claiming to make, 
 then execute terraform apply to commit and start the build
 */
+
+
+
+
+provider "aws" {
+    region = "us-east-2"
+    access_key = "AX******"
+    secret_key = ""
+    #profile = "default"
+}
+
+resource "aws_security_group" "web-sg-01" {
+    name = "web-sg-01"
+    ingress {
+        protocol = "tcp"
+        from_port = "8080"
+        to_port = "8080"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+/* resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("terraform-demo.pub")}"
+} */
+
+resource "aws_instance" "dev0-web-01" {
+    ami = "ami-*****"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["aws_security_group.web-sg-01.id"]
+    user_data = <<-EOF
+    /* #!/bin/bash */
+    /* echo "This is running successfully by terraform" > index.html
+    nohup busybox httpd -f -p 8080 & */
+    #! /bin/bash
+        sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+    EOF
+    tags = {
+      "Name" = "dev0-web-01"
+    }
+}
+
+output "dev0-web-01-pubip" {
+    value = "aws_instance.dev0-web-01.public_ip"
+  
+}
